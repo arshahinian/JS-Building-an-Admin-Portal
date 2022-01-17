b793cae3c8c8a5ae738e131c04181b116d820a24
Araz Shahinian <arshahinian@gmail.com>
2022-01-17 17:55:19 -0500

    fixed some bugs

--- public/admin.js
+++ public/admin.js
@@ -9,27 +9,31 @@ async function mainAdmin()
 }
 
 async function updateQty(elementId,bookId,bookTitle){
-    alert(bookTitle);
+    console.log('Book Title: ' + bookTitle);
     let textBoxItem = document.querySelector(`#${elementId}`)
-    let valueItem = textBoxItem.value;
-    
-    
-    if (!isNaN(valueItem))
+    let newQty = textBoxItem.value;
+    console.log('New Qty: ' + newQty);
+
+    if(newQty === null || newQty.length <= 0 || isNaN(newQty))
+    {
+        alert("Please put a valid value in as the quantity!");
+    }
+    else    
     {
         let jsonText = JSON.stringify(
             {
                 id: parseInt(bookId) 
-                ,quantity: parseInt(valueItem)
-            })
-        alert(jsonText);
+                ,quantity: parseInt(newQty)
+            });
+        console.log('JSON Text: ' + jsonText);
         let response = await fetch('http://localhost:3001/updateBook',{
             method:'PATCH',
             headers:{
                 'Content-Type':'application/json'
             },
             body: jsonText,
-        })
-        let updateBook = await response.json()
+        });
+        let updateBook = await response.json();
         console.log(updateBook);
     }
 }
@@ -38,8 +42,10 @@ async function renderBook(book) {
     let bookContainer = document.querySelector('#book-container')
     bookContainer.innerHTML += `
         <li class="col-sm-3">
-            <span class="card-title">${book.title}</span> 
+            <span class="card-title">${book.title}</span>
+            <br/>
             <input type="textbox" id="book_id_${book.id}"></input> 
+            <br/>
             <input type="submit" onclick='updateQty("book_id_${book.id}","${book.id}","${book.title}")'></input>         
         </li>
     `
